217265b1422f0dc3e9c49b7923da9d8eedb5de82
Aleksey170999 <alexbooster3@yandex.ru>
2025-09-10 23:48:50 +0300

    add db connection

--- internal/config/db/db.go
+++ internal/config/db/db.go
@@ -4,15 +4,34 @@ import (
 	"database/sql"
 	"fmt"
 
-	_ "github.com/jackc/pgx/v5/stdlib"
+	_ "github.com/lib/pq"
+
+	"github.com/pressly/goose/v3"
 )
 
 func PingDB(databaseDSN string) error {
-	db, err := sql.Open("pgx", databaseDSN)
+	db, err := sql.Open("postgres", databaseDSN)
+
 	if err != nil {
 		return fmt.Errorf("unable to connect to DB: %w", err)
 	}
+	if err := db.Ping(); err != nil {
+		return fmt.Errorf("unable to ping DB: %w", err)
+	}
 	defer db.Close()
 
+	return nil
+}
+
+func ApplyMigrations(db *sql.DB) error {
+	if err := goose.SetDialect("postgres"); err != nil {
+		return fmt.Errorf("failed to set dialect: %w", err)
+	}
+
+	if err := goose.Up(db, "./migrations/"); err != nil {
+		return fmt.Errorf("failed to apply migrations: %w", err)
+	}
+
+	fmt.Println("Migrations applied successfully!")
 	return nil
 }
